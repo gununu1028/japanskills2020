0c947c361cc0650e672978433f82a4f34eacdc42
Atsushi Matsuda <akiyam1028@gmail.com>
2020-10-12 23:53:53 +0900

    注文作成のアクションを用意

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\GoodsController;
+use App\Http\Controllers\OrdersController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,3 +20,4 @@ Route::get('/', function () {
 });
 
 Route::resource('goods', GoodsController::class);
+Route::resource('orders',OrdersController::class);
