183b44d6ff9aa8260537cca7f37aea81094fe3c5
MatsudaAtsushi <akiyam1028@gmail.com>
2020-11-01 00:36:27 +0900

    シードデータ追加

--- database/seeders/ItemsTableSeeder.php
+++ database/seeders/ItemsTableSeeder.php
@@ -17,10 +17,21 @@ class ItemsTableSeeder extends Seeder
         // 1つ目
         $param = [
             'shop_id' => 1,
-            'photo' => 'http://skilljapan.info/images/shop.png',
+            'photo' => 'https://asahi-curation.s3.ap-northeast-1.amazonaws.com/wp-content/uploads/2019/02/15162001/z1902k18p01.jpg',
             'name' => 'クラシックホットチョコレート',
             'price' => '650',
             'description'=> 'コク深く、なめらかな味わいの70%ダークチョコレートを、ミルクで仕上げました。'
+        ];
+        $i = new Item();
+        $i->fill($param)->save();
+        
+        // 2つ目
+        $param = [
+            'shop_id' => 1,
+            'photo' => 'https://i.pinimg.com/originals/66/09/76/660976653156a32d3f273fa9ccc2f135.jpg',
+            'name' => 'スパイシーホットチョコレート',
+            'price' => '650',
+            'description'=> '厳選したカカオを使用したホットチョコレート。チポレチリ、シナモン、ナツメグなど、7種類のスパイスが入った刺激的な癖になるお味。'
         ];
         $i = new Item();
         $i->fill($param)->save();
